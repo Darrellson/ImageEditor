70b7922f4b6321c21eb0abbfd5e262ab66fedb70
Darrellson <muraddiasamidze86@gmail.com>
2024-05-17 13:52:57 +0400

    124

--- script.js
+++ script.js
@@ -42,19 +42,8 @@ const activateDrawing = (shape) => {
     origX = pointer.x;
     origY = pointer.y;
 
-    switch (shape) {
-      case "rect":
-        drawRectangle(origX, origY);
-        break;
-      case "circle":
-        drawCircle(origX, origY);
-        break;
-      case "arrow":
-        drawArrow(origX, origY);
-        break;
-      case "text":
-        addText(origX, origY);
-        break;
+    if (shape === "rect") {
+      drawRectangle(origX, origY);
     }
   });
 
@@ -62,16 +51,8 @@ const activateDrawing = (shape) => {
     if (!isDrawing) return;
     const pointer = canvas.getPointer(o.e);
 
-    switch (shape) {
-      case "rect":
-        adjustRectangleSize(pointer);
-        break;
-      case "circle":
-        adjustCircleSize(pointer);
-        break;
-      case "arrow":
-        adjustArrow(pointer);
-        break;
+    if (shape === "rect") {
+      adjustRectangleSize(pointer);
     }
 
     canvas.renderAll();
@@ -104,43 +85,6 @@ const drawRectangle = (x, y) => {
 };
 
 /**
- * Draws a circle at the given coordinates.
- * @param {number} x - The x-coordinate.
- * @param {number} y - The y-coordinate.
- */
-const drawCircle = (x, y) => {
-  const circle = new fabric.Circle({
-    left: x,
-    top: y,
-    radius: 1,
-    fill: selectedColor,
-    stroke: selectedColor,
-    strokeWidth: 2,
-    selectable: true,
-  });
-  canvas.add(circle);
-  activeObject = circle;
-};
-
-/**
- * Draws an arrow at the given coordinates.
- * @param {number} x - The x-coordinate.
- * @param {number} y - The y-coordinate.
- */
-const drawArrow = (x, y) => {
-  const points = [x, y, x, y];
-  const arrow = new fabric.Line(points, {
-    strokeWidth: 2,
-    fill: selectedColor,
-    stroke: selectedColor,
-    type: "arrow",
-    selectable: true,
-  });
-  canvas.add(arrow);
-  activeObject = arrow;
-};
-
-/**
  * Adjusts the size of the rectangle during drawing.
  * @param {object} pointer - The pointer object.
  */
@@ -151,45 +95,11 @@ const adjustRectangleSize = (pointer) => {
 };
 
 /**
- * Adjusts the size of the circle during drawing.
- * @param {object} pointer - The pointer object.
- */
-const adjustCircleSize = (pointer) => {
-  const radius = Math.abs(origX - pointer.x) / 2;
-  activeObject.set({ radius });
-};
-
-/**
- * Adjusts the position of the arrow during drawing.
- * @param {object} pointer - The pointer object.
- */
-const adjustArrow = (pointer) => {
-  activeObject.set({ x2: pointer.x, y2: pointer.y });
-};
-
-/**
- * Prompts the user to enter text and adds it to the canvas.
- * @param {number} x - The x-coordinate.
- * @param {number} y - The y-coordinate.
- */
-const addText = (x, y) => {
-  const textVal = prompt("Please enter text value..", "");
-  if (textVal && textVal.trim() !== "") {
-    const text = new fabric.Text(textVal, {
-      left: x,
-      top: y,
-      selectable: true,
-    });
-    canvas.add(text);
-  }
-};
-
-/**
  * Updates the color of all shapes on the canvas.
  */
 const updateShapesColor = () => {
   canvas.getObjects().forEach((obj) => {
-    if (obj.type === "rect" || obj.type === "circle" || obj.type === "line") {
+    if (obj.type === "rect") {
       obj.set({ fill: selectedColor, stroke: selectedColor });
     }
   });
@@ -205,18 +115,15 @@ document.getElementById("imageLoader").addEventListener("change", (e) => {
   reader.onload = (f) => {
     const data = f.target.result;
     fabric.Image.fromURL(data, (img) => {
-      const oImg = img
-        .set({
-          left: 0,
-          top: 0,
-          angle: 0,
-          scaleX: canvas.width / img.width,
-          scaleY: canvas.height / img.height,
-          selectable: true,
-        })
-        .scale(0.5);
-      canvas.add(oImg).renderAll();
-      canvas.setActiveObject(oImg);
+      img.set({
+        left: 0,
+        top: 0,
+        selectable: false, // Make the image unselectable
+      });
+      img.scaleToWidth(canvas.width); // Scale the image to canvas width
+      img.scaleToHeight(canvas.height); // Scale the image to canvas height
+      canvas.add(img).sendToBack(); // Add image to the canvas and send it to back
+      canvas.renderAll();
     });
   };
   reader.readAsDataURL(file);
